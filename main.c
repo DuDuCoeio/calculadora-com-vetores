1b88da507ca279ab7f8c7331d767ae0a8c9fdc59
DuDuCoeio <130939493+DuDuCoeio@users.noreply.github.com>
2023-05-22 11:21:03 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+// faça um programa que execute as 4 opreações aritimeticas usando ponteiro
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL,"portuguese");
+	 int n1,n2;
+	 int *p1,*p2;
+	
+	printf("digite o primeiro valor:");	 
+	  scanf("%d", &n1);					    
+	printf("digite o segundo valor:");
+	  scanf("%d", &n2);
+	 
+	   p1=&n1;
+	   p2=&n2;
+	
+	printf("soma: %d\n",*p1 + *p2);
+ 	 printf("subtração: %d\n",*p1 - *p2);
+	  printf("divisão: %d\n",*p1 / *p2);
+	   printf("multiplicação: %d\n",*p1 * *p2);
+}
+*/
+//criar um programa que recebe uma palavra de 5 carateres onde troque o A por U 
+
+
